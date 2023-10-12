b51b022716a13cfe133eb3be3d17fb16eb5826a6
Cao Duc Trung <113839491+trung8134@users.noreply.github.com>
2023-10-12 11:59:47 +0700

    update

--- dataset.py
+++ dataset.py
@@ -1,36 +1,35 @@
-import os
-import cv2 
-import numpy as np
-from sklearn.model_selection import train_test_split
-from tensorflow.keras.utils import to_categorical
+from keras.preprocessing.image import ImageDataGenerator
 
-def array_dataset(img_size):
-    def load_images_from_folder(path, dim):
-        data = []
-        labels = []
+def tf_dataset(img_size, batch_size):
+    dataset_path = 'Datasets/training'
 
-        global category 
-        category = {'Corn___Common_Rust': 0, 'Corn___Gray_Leaf_Spot': 1, 'Corn___Healthy': 2, 'Corn___Northern_Leaf_Blight': 3, 'Rice___Brown_Spot': 4, 'Rice___Healthy': 5, 'Rice___Leaf_Blast': 6, 'Rice___Neck_Blast': 7, 'Wheat___Brown_Rust': 8, 'Wheat___Healthy': 9, 'Wheat___Yellow_Rust': 10}
+    # Data generators
+    train_datagen = ImageDataGenerator(rescale=1./255, validation_split=0.2)
 
-        for i in sorted(os.listdir(path)):
-            path_class = os.path.join(path, i)
-            for j in os.listdir(path_class):
-                # Đọc ảnh ở chế độ màu RGB
-                img_rgb = cv2.imread(os.path.join(path_class, j), cv2.IMREAD_COLOR)
-                img_rgb_resized = cv2.resize(img_rgb, (dim, dim))
-
-                data.append(img_rgb_resized)
-                labels.append(category[i]) 
-
-        return data, labels
-
-    dim = img_size
-    path = 'Datasets/training'
-
-    x, y = load_images_from_folder(path, dim)
-    x = np.array(x)
-    y = to_categorical(y, num_classes=len(category)) 
-
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
-
-    return x_train, x_test, y_train, y_test
+    train_batches = train_datagen.flow_from_directory(
+        dataset_path,
+        target_size=img_size,
+        batch_size=batch_size,
+        class_mode='categorical',
+        subset='training'
+    )
+    
+    valid_batches = train_datagen.flow_from_directory(
+        dataset_path,
+        target_size=img_size,
+        batch_size=batch_size,
+        class_mode='categorical',
+        subset='validation'
+    )
+    
+    test_batches = train_datagen.flow_from_directory(
+        dataset_path,
+        target_size=img_size,
+        batch_size=batch_size,
+        class_mode='categorical',
+        shuffle=False,
+        subset='validation'
+    )
+    
+    return train_batches, valid_batches, test_batches
+    
--- models.py
+++ models.py
@@ -1,6 +1,4 @@
 from tensorflow import keras
-from augmentations import augment_data
-from vit_keras import vit
 from classification_models.tfkeras import Classifiers
 
 def create_encoder_ResNet18(img_size, num_classes):
@@ -16,8 +14,7 @@ def create_encoder_ResNet18(img_size, num_classes):
         )
 
     inputs = keras.Input(shape=(img_size, img_size, 3))
-    augment = augment_data(inputs)
-    outputs = resnet(augment)
+    outputs = resnet(inputs)
     model = keras.Model(inputs=inputs, outputs=outputs, name="Encoder-ResNet18")
     
     return model
@@ -34,27 +31,8 @@ def create_encoder_ResNet50(img_size, num_classes):
         )
 
     inputs = keras.Input(shape=(img_size, img_size, 3))
-    augment = augment_data(inputs)
-    outputs = resnet(augment)
+    outputs = resnet(inputs)
     model = keras.Model(inputs=inputs, outputs=outputs, name="Encoder-ResNet50")
-    
-    return model
-
-
-def create_encoder_ViTB16(img_size, num_classes):
-    vit_model = vit.vit_b16(
-        image_size = img_size, # Kích thước ảnh đầu vào
-        activation = 'softmax',
-        pretrained = True,
-        include_top = False, # Thêm lớp MLP sau lớp Transformer
-        pretrained_top = False, # Không sử dụng weights đã train trên tập data gốc lên lớp MLP sau lớp Transformer
-        classes = num_classes
-    )
-
-    inputs = keras.Input(shape=(img_size, img_size, 3))
-    augment = augment_data(inputs)
-    outputs = vit_model(augment)
-    model = keras.Model(inputs=inputs, outputs=outputs, name="Encoder-ViTB16")
     
     return model
 
