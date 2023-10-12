3be09108382b9a8774d56749bcadf98adc664b23
Cao Duc Trung <113839491+trung8134@users.noreply.github.com>
2023-10-12 12:10:39 +0700

    upload

--- dataset.py
+++ dataset.py
@@ -1,14 +1,14 @@
 from keras.preprocessing.image import ImageDataGenerator
 
 def tf_dataset(img_size, batch_size):
-    dataset_path = 'Datasets/training'
+    dataset_path = 'C:/Users/caotr/D. Computer Science/Data Science/DL/Project/NCKH-2024/Datasets/training'
 
     # Data generators
     train_datagen = ImageDataGenerator(rescale=1./255, validation_split=0.2)
 
     train_batches = train_datagen.flow_from_directory(
         dataset_path,
-        target_size=img_size,
+        target_size=(img_size, img_size),
         batch_size=batch_size,
         class_mode='categorical',
         subset='training'
@@ -16,7 +16,7 @@ def tf_dataset(img_size, batch_size):
     
     valid_batches = train_datagen.flow_from_directory(
         dataset_path,
-        target_size=img_size,
+        target_size=(img_size, img_size),
         batch_size=batch_size,
         class_mode='categorical',
         subset='validation'
@@ -24,7 +24,7 @@ def tf_dataset(img_size, batch_size):
     
     test_batches = train_datagen.flow_from_directory(
         dataset_path,
-        target_size=img_size,
+        target_size=(img_size, img_size),
         batch_size=batch_size,
         class_mode='categorical',
         shuffle=False,
