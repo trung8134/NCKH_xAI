12a051dc02bbc8009fe517049094803ceb47e5a6
Cao Duc Trung <113839491+trung8134@users.noreply.github.com>
2023-10-12 13:52:31 +0700

    update

--- dataset.py
+++ dataset.py
@@ -1,13 +1,14 @@
 from keras.preprocessing.image import ImageDataGenerator
 
 def tf_dataset(img_size, batch_size):
-    dataset_path = 'C:/Users/caotr/D. Computer Science/Data Science/DL/Project/NCKH-2024/Datasets/training'
-
+    # C:/Users/caotr/D. Computer Science/Data Science/DL/Project/NCKH-2024/
+    train_path = 'Datasets/data/training'
+    test_path = 'Datasets/data/testing'
     # Data generators
-    train_datagen = ImageDataGenerator(rescale=1./255, validation_split=0.2)
+    train_datagen = ImageDataGenerator(rescale=1./255, validation_split=0.1)
 
     train_batches = train_datagen.flow_from_directory(
-        dataset_path,
+        train_path,
         target_size=(img_size, img_size),
         batch_size=batch_size,
         class_mode='categorical',
@@ -15,7 +16,7 @@ def tf_dataset(img_size, batch_size):
     )
     
     valid_batches = train_datagen.flow_from_directory(
-        dataset_path,
+        train_path,
         target_size=(img_size, img_size),
         batch_size=batch_size,
         class_mode='categorical',
@@ -23,12 +24,10 @@ def tf_dataset(img_size, batch_size):
     )
     
     test_batches = train_datagen.flow_from_directory(
-        dataset_path,
+        test_path,
         target_size=(img_size, img_size),
         batch_size=batch_size,
         class_mode='categorical',
-        shuffle=False,
-        subset='validation'
     )
     
     return train_batches, valid_batches, test_batches
