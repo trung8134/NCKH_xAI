4f6a02ebeb00a58159ff68f8b0d7ece0b914ec42
Cao Duc Trung <113839491+trung8134@users.noreply.github.com>
2024-01-10 22:54:30 +0700

    update

--- model_transfer/models.py
+++ model_transfer/models.py
@@ -1,6 +1,5 @@
 import tensorflow.keras.backend as K
 import tensorflow as tf
-import tensorflow_addons as tfa
 from tensorflow.keras import layers as L
 from tensorflow import keras
 from keras import layers
@@ -9,6 +8,7 @@ from keras.models import Model
 from model_transfer.config import MobileViT 
 
 def MobileViT_S(input_shape, class_count):
+    K.clear_session()
     num_channels = [16, 32, 64, 64, 64, 96, 144, 128, 192, 160, 240, 640]
     dim = [144, 192, 240]
     expansion_ratio = 4
@@ -21,11 +21,24 @@ def MobileViT_S(input_shape, class_count):
         num_classes=class_count
     )
     
-    model.compile(optimizer=tfa.optimizers.AdamW(learning_rate=0.002), loss=keras.losses.categorical_crossentropy(label_smoothing=0.1), metrics=['accuracy'])
+    last_layer = model.get_layer('conv2d_27')
+    last_output = last_layer.output
+    
+    x = layers.BatchNormalization()(last_output)
+    layers.Activation('swish')(x)
+    x = layers.GlobalAveragePooling2D()(x)
+    x = layers.Dense(class_count, activation='softmax')(x)
+    
+    model = Model(model.input, x)
+    
+    optimizer = keras.optimizers.AdamW(learning_rate=0.002, weight_decay=0.01)
+    loss_function = keras.losses.CategoricalCrossentropy(label_smoothing=0.1)
+    model.compile(optimizer=optimizer, loss=loss_function, metrics=['accuracy'])
     
     return model
 
 def MobileViT_XS(input_shape, class_count):
+    K.clear_session()
     num_channels = [16, 32, 48, 48, 48, 64, 96, 80, 120, 96, 144, 384]
     dim = [96, 120, 144]
     expansion_ratio = 4
@@ -39,11 +52,24 @@ def MobileViT_XS(input_shape, class_count):
         num_classes=class_count
     )
     
-    model.compile(optimizer=tfa.optimizers.AdamW(learning_rate=0.002), loss=keras.losses.categorical_crossentropy(label_smoothing=0.1), metrics=['accuracy'])
+    last_layer = model.get_layer('conv2d_27')
+    last_output = last_layer.output
+    
+    x = layers.BatchNormalization()(last_output)
+    layers.Activation('swish')(x)
+    x = layers.GlobalAveragePooling2D()(x)
+    x = layers.Dense(class_count, activation='softmax')(x)
+    
+    model = Model(model.input, x)
+    
+    optimizer = keras.optimizers.AdamW(learning_rate=0.002, weight_decay=0.01)
+    loss_function = keras.losses.CategoricalCrossentropy(label_smoothing=0.1)
+    model.compile(optimizer=optimizer, loss=loss_function, metrics=['accuracy'])
     
     return model
 
 def MobileViT_XXS(input_shape, class_count):
+    K.clear_session()
     num_channels = [16, 16, 24, 24, 24, 48, 64, 64, 80, 80, 96, 320]
     dim = [64, 80, 96]
     expansion_ratio = 2
@@ -56,7 +82,19 @@ def MobileViT_XXS(input_shape, class_count):
         num_classes=class_count
     )
     
-    model.compile(optimizer=tfa.optimizers.AdamW(learning_rate=0.002), loss=keras.losses.categorical_crossentropy(label_smoothing=0.1), metrics=['accuracy'])
+    last_layer = model.get_layer('conv2d_27')
+    last_output = last_layer.output
+    
+    x = layers.BatchNormalization()(last_output)
+    layers.Activation('swish')(x)
+    x = layers.GlobalAveragePooling2D()(x)
+    x = layers.Dense(class_count, activation='softmax')(x)
+    
+    model = Model(model.input, x)
+    
+    optimizer = keras.optimizers.AdamW(learning_rate=0.002, weight_decay=0.01)
+    loss_function = keras.losses.CategoricalCrossentropy(label_smoothing=0.1)
+    model.compile(optimizer=optimizer, loss=loss_function, metrics=['accuracy'])
     
     return model
     
