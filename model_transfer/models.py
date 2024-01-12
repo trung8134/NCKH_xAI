807d62b6a091b0a46f91ec4d524ddc15b580c5f4
Cao Duc Trung <113839491+trung8134@users.noreply.github.com>
2024-01-13 02:12:43 +0700

    Update models.py

--- model_transfer/models.py
+++ model_transfer/models.py
@@ -49,40 +49,56 @@ def VGG16ViT_model(img_shape, class_count):
     base_model = vgg16.VGG16(input_shape=img_shape, include_top=False, weights="imagenet")
     
     for layer in base_model.layers:
-        layer.trainable = False
+    layer.trainable = False
     
     # Get the output of block4_conv3
-    block4_conv3_output = base_model.get_layer('block4_conv3').output
+    block1_conv2_output = base_model.get_layer('block1_conv2').output
     
     # Attention mechanism
-    query_cnn_layer = layers.Conv2D(filters=512, kernel_size=(3, 3), padding='same', activation='relu', name='conv_attention_1')
-    query_seq_encoding = query_cnn_layer(block4_conv3_output)
+    query_cnn_layer = layers.Conv2D(filters=64, kernel_size=(3, 3), padding='same', activation='relu', name='conv_attention_1')
+    query_seq_encoding = query_cnn_layer(block1_conv2_output)
     
-    value_cnn_layer = layers.Conv2D(filters=512, kernel_size=(3, 3), padding='same', activation='relu', name='conv_attention_2')
-    value_seq_encoding = value_cnn_layer(block4_conv3_output)
+    value_cnn_layer = layers.Conv2D(filters=64, kernel_size=(3, 3), padding='same', activation='relu', name='conv_attention_2')
+    value_seq_encoding = value_cnn_layer(block1_conv2_output)
     
     query_value_attention_seq = layers.Attention()([query_seq_encoding, value_seq_encoding])
     
     # MaxPooling2D for query_value_attention_seq.
-    pooling_behind_attention = tensorflow.keras.layers.MaxPooling2D(name='block4_pool')(query_value_attention_seq)
+    pooling_behind_attention = layers.MaxPooling2D(name='block1_pool')(query_value_attention_seq)
     
-    block5_conv1_layer = base_model.get_layer('block5_conv1')(pooling_behind_attention)
+    # block2
+    block2_conv1_layer = base_model.get_layer('block2_conv1')(pooling_behind_attention)
+    block2_conv2_layer = base_model.get_layer('block2_conv2')(block2_conv1_layer)
+    pooling2 = layers.MaxPooling2D(name='block2_pool')(block2_conv2_layer)
+    
+    # block3
+    block3_conv1_layer = base_model.get_layer('block3_conv1')(pooling2)
+    block3_conv2_layer = base_model.get_layer('block3_conv2')(block3_conv1_layer)
+    block3_conv3_layer = base_model.get_layer('block3_conv3')(block3_conv2_layer)
+    pooling3 = layers.MaxPooling2D(name='block3_pool')(block3_conv3_layer)
+    
+    # block4
+    block4_conv1_layer = base_model.get_layer('block4_conv1')(pooling3)
+    block4_conv2_layer = base_model.get_layer('block4_conv2')(block4_conv1_layer)
+    block4_conv3_layer = base_model.get_layer('block4_conv3')(block4_conv2_layer)
+    pooling4 = layers.MaxPooling2D(name='block4_pool')(block4_conv3_layer)
+    
+    # block5
+    block5_conv1_layer = base_model.get_layer('block5_conv1')(pooling4)
     block5_conv2_layer = base_model.get_layer('block5_conv2')(block5_conv1_layer)
     block5_conv3_layer = base_model.get_layer('block5_conv3')(block5_conv2_layer)
-    
-    # MaxPooling2D for block5
-    pooling_block5 = layers.MaxPooling2D(name='block5_pool')(block5_conv3_layer)
+    pooling5 = layers.MaxPooling2D(name='block5_pool')(block5_conv3_layer)
     
     # Flatten
-    flatten = layers.Flatten()(pooling_block5)
+    flatten = layers.Flatten()(pooling5)
     
     # output layer 
-    output_layer = layers.Dense(class_count, activation='softmax')(flatten)
+    output_layer = layers.Dense(11, activation='softmax')(flatten)
     
     # Create the final model
     model = Model(inputs=base_model.input, outputs=output_layer)
-
-    model.compile(optimizer=keras.optimizers.Adam(learning_rate=0.001), loss='categorical_crossentropy', metrics=['accuracy'])
+    
+    model.compile(optimizer=keras.optimizers.Adam(learning_rate=0.0001), loss='categorical_crossentropy', metrics=['accuracy'])
     return model
 
 # EfficientNetV2S
