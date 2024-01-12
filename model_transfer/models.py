d77a3a7ab642f4a2475eb3a0ae57983b5e6cab12
Cao Duc Trung <113839491+trung8134@users.noreply.github.com>
2024-01-12 21:54:42 +0700

    updates VGG16ViT

--- model_transfer/models.py
+++ model_transfer/models.py
@@ -1,103 +1,10 @@
 import tensorflow.keras.backend as K
-import tensorflow as tf
+import tensorflow 
 from tensorflow.keras import layers as L
 from tensorflow import keras
 from keras import layers
 from keras.applications import resnet50, vgg16, EfficientNetV2S
 from keras.models import Model
-from model_transfer.config import MobileViT 
-
-def MobileViT_S(input_shape, class_count):
-    K.clear_session()
-    num_channels = [16, 32, 64, 64, 64, 96, 144, 128, 192, 160, 240, 640]
-    dim = [144, 192, 240]
-    expansion_ratio = 4
-
-    model = MobileViT(
-        input_shape,
-        num_channels,
-        dim,
-        expansion_ratio,
-        num_classes=class_count
-    )
-    
-    last_layer = model.get_layer('conv2d_27')
-    last_output = last_layer.output
-    
-    x = layers.BatchNormalization()(last_output)
-    layers.Activation('swish')(x)
-    x = layers.GlobalAveragePooling2D()(x)
-    x = layers.Dense(class_count, activation='softmax')(x)
-    
-    model = Model(model.input, x)
-    
-    optimizer = keras.optimizers.AdamW(weight_decay=0.01, learning_rate=0.002)
-    loss_function = keras.losses.CategoricalCrossentropy(label_smoothing=0.1)
-    model.compile(optimizer=optimizer, loss=loss_function, metrics=['accuracy'])
-    
-    return model
-
-def MobileViT_XS(input_shape, class_count):
-    K.clear_session()
-    num_channels = [16, 32, 48, 48, 48, 64, 96, 80, 120, 96, 144, 384]
-    dim = [96, 120, 144]
-    expansion_ratio = 4
-
-
-    model = MobileViT(
-        input_shape,
-        num_channels,
-        dim,
-        expansion_ratio,
-        num_classes=class_count
-    )
-    
-    last_layer = model.get_layer('conv2d_27')
-    last_output = last_layer.output
-    
-    x = layers.BatchNormalization()(last_output)
-    layers.Activation('swish')(x)
-    x = layers.GlobalAveragePooling2D()(x)
-    x = layers.Dense(class_count, activation='softmax')(x)
-    
-    model = Model(model.input, x)
-    
-    optimizer = keras.optimizers.AdamW(weight_decay=0.01, learning_rate=0.002)
-    loss_function = keras.losses.CategoricalCrossentropy(label_smoothing=0.1)
-    model.compile(optimizer=optimizer, loss=loss_function, metrics=['accuracy'])
-    
-    return model
-
-def MobileViT_XXS(input_shape, class_count):
-    K.clear_session()
-    num_channels = [16, 16, 24, 24, 24, 48, 64, 64, 80, 80, 96, 320]
-    dim = [64, 80, 96]
-    expansion_ratio = 2
-
-    model = MobileViT(
-        input_shape,
-        num_channels,
-        dim,
-        expansion_ratio,
-        num_classes=class_count
-    )
-    
-    last_layer = model.get_layer('conv2d_27')
-    last_output = last_layer.output
-    
-    x = layers.BatchNormalization()(last_output)
-    layers.Activation('swish')(x)
-    x = layers.GlobalAveragePooling2D()(x)
-    x = layers.Dense(class_count, activation='softmax')(x)
-    
-    model = Model(model.input, x)
-    
-    optimizer = keras.optimizers.AdamW(weight_decay=0.01, learning_rate=0.002)
-    loss_function = keras.losses.CategoricalCrossentropy(label_smoothing=0.1)
-    model.compile(optimizer=optimizer, loss=loss_function, metrics=['accuracy'])
-    
-    return model
-    
 
 # ResNet
 def ResNet50_model(img_shape, class_count):
@@ -136,6 +43,48 @@ def VGG16_model(img_shape, class_count):
     
     return model
 
+# VGGViT
+def VGG16ViT_model(img_shape, class_count):
+    K.clear_session()
+    base_model = vgg16.VGG16(input_shape=img_shape, include_top=False, weights="imagenet")
+    
+    for layer in base_model.layers:
+        layer.trainable = False
+    
+    # Get the output of block4_conv3
+    block4_conv3_output = base_model.get_layer('block4_conv3').output
+    
+    # Attention mechanism
+    query_cnn_layer = layers.Conv2D(filters=512, kernel_size=(3, 3), padding='same', activation='relu', name='conv_attention_1')
+    query_seq_encoding = query_cnn_layer(block4_conv3_output)
+    
+    value_cnn_layer = layers.Conv2D(filters=512, kernel_size=(3, 3), padding='same', activation='relu', name='conv_attention_2')
+    value_seq_encoding = value_cnn_layer(block4_conv3_output)
+    
+    query_value_attention_seq = layers.Attention()([query_seq_encoding, value_seq_encoding])
+    
+    # MaxPooling2D for query_value_attention_seq.
+    pooling_behind_attention = layers.MaxPooling2D(name='block4_pool')(query_value_attention_seq)
+    
+    block5_conv1_layer = base_model.get_layer('block5_conv1')(pooling_behind_attention)
+    block5_conv2_layer = base_model.get_layer('block5_conv2')(block5_conv1_layer)
+    block5_conv3_layer = base_model.get_layer('block5_conv3')(block5_conv2_layer)
+    
+    # MaxPooling2D for block5
+    pooling_block5 = layers.MaxPooling2D(name='block5_pool')(block5_conv3_layer)
+    
+    # Flatten
+    flatten = layers.Flatten()(pooling_block5)
+    
+    # output layer 
+    output_layer = layers.Dense(class_count, activation='softmax')(flatten)
+    
+    # Create the final model
+    model = Model(inputs=base_model.input, outputs=output_layer)
+
+    model.compile(optimizer=keras.optimizers.Adam(learning_rate=0.001), loss='categorical_crossentropy', metrics=['accuracy'])
+    return model
+
 # EfficientNetV2S
 def EfficientNetV2S_model(img_shape, class_count):
     base_model = EfficientNetV2S(input_shape=img_shape, include_top=False, weights="imagenet")
@@ -151,6 +100,6 @@ def EfficientNetV2S_model(img_shape, class_count):
     x = layers.Dense(class_count, activation='softmax')(x)
     
     model = Model(base_model.input, x) 
-    model.compile(optimizer=keras.optimizers.Adam(learning_rate=0.001), loss='categorical_crossentropy', metrics=['accuracy'])
+    model.compile(optimizer=keras.optimizers.Adam(learning_rate=0.0001), loss='categorical_crossentropy', metrics=['accuracy'])
     
     return model
